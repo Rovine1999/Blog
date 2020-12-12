8449c26f1902e367db809d9ae70160e22c7e3556
Rovine1999 <rovinewanjala99@gmail.com>
2020-12-12 20:19:05 +0300

    add __init__.py to initialise the app

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,20 @@
 from flask import Blueprint
 
-auth = Blueprint('auth',__name__)
+bp = Blueprint('auth',__name__)
 
-from . import views
+from . import views, errors
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
